78d926466523e184d4a3a5a4acceb20a368798da
butchtabios <butchtabios@yahoo.com>
2016-01-24 19:16:36 +0800

    convert tabs to spaces

--- examples/GSM_GPRSLibrary_SMS/GSM_GPRSLibrary_SMS.ino
+++ examples/GSM_GPRSLibrary_SMS/GSM_GPRSLibrary_SMS.ino
@@ -23,45 +23,47 @@ const int GSM_ON_pin = 7;
 
 void setup()
 {
-     //Serial connection.
-     Serial.begin(9600);
-     Serial.println("GSM Shield testing.");
+  //Serial connection.
+  Serial.begin(9600);
+  Serial.println("GSM Shield testing.");
 
-	 //Configure Comm Port to select Hardware or Software serial
-	 //gsm.SelectHardwareSerial(&Serial, GSM_ON_pin);
-	 gsm.SelectSoftwareSerial(RX_pin, TX_pin, GSM_ON_pin);
+  //Configure Comm Port to select Hardware or Software serial
+  //gsm.SelectHardwareSerial(&Serial, GSM_ON_pin);
+  gsm.SelectSoftwareSerial(RX_pin, TX_pin, GSM_ON_pin);
 
-	 //Configure baudrate.
-	 if (gsm.begin(9600)) {
-          Serial.println("\nstatus=READY");
-          started=true;
-     } else Serial.println("\nstatus=IDLE");
+  //Configure baudrate.
+  if (gsm.begin(9600)) 
+  {
+    Serial.println("\nstatus=READY");
+    started=true;
+  } else Serial.println("\nstatus=IDLE");
 
-     if(started) {
-          //Enable this two lines if you want to send an SMS.
-          if (sms.SendSMS(n, "Arduino SMS"))
-			Serial.println("\nSMS sent OK");
-     }
-
+  if(started) 
+  {
+    //Enable this two lines if you want to send an SMS.
+    if (sms.SendSMS(n, "Arduino SMS"))
+      Serial.println("\nSMS sent OK");
+  }
 };
 
 void loop()
 {
-     if(started) {
-          //Read if there are messages on SIM card and print them.
-        /**  deprecated method
-        if(gsm.readSMS(smsbuffer, 160, n, 20)) {
-               Serial.println(n);
-               Serial.println(smsbuffer);
-          }
-          **/
-          //get 1st sms
-		 if (sms.GetSMS(1, n, 20, smsbuffer, 160))
-		 {
-			 Serial.println(n);
-			 Serial.println(smsbuffer);
-			 sms.DeleteSMS(1);
-		 }
-          delay(1000);
-     }
+  if(started) 
+  {
+    //Read if there are messages on SIM card and print them.
+    /**  deprecated method
+    if(gsm.readSMS(smsbuffer, 160, n, 20)) {
+       Serial.println(n);
+       Serial.println(smsbuffer);
+    }
+    **/
+    //get 1st sms
+    if (sms.GetSMS(1, n, 20, smsbuffer, 160))
+    {
+      Serial.println(n);
+      Serial.println(smsbuffer);
+      sms.DeleteSMS(1);
+    }
+    delay(1000);
+  }
 };
